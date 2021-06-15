f9c4942cdaf6e69be2a6d4070fc8da445994b783
Aviral Tandon <201951036@iiitvadodara.ac.in>
2021-06-15 20:55:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,6 +45,15 @@ const App = () => {
       <Movielottie className={classes.movieLogo}/>
       </div>
         <MovieCards movies={latestMovies} activeMovie={activeMovie}/>
+          <footer className="Footer">
+        <p className="Footer__copyright">
+          &copy; 2021 <span>Aviral Tandon</span>
+        </p>
+        <p className="Footer__message">
+          <strong className="Footer__bold">&lt;/&gt;</strong> with{" "}
+          <span className="Footer__bold">🧡</span>
+        </p>
+      </footer>
       </div>
     }
     </div>
