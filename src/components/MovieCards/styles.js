dcb9b310cb30944ebc277d6886bc4bd38113698b
Aviral Tandon <201951036@iiitvadodara.ac.in>
2021-06-15 20:45:26 +0530

    Update styles.js

--- src/components/MovieCards/styles.js
+++ src/components/MovieCards/styles.js
@@ -1,18 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-  // card: {
-  //   display: 'flex',
-  //   flexDirection: 'column',
-  //   justifyContent: 'space-between',
-  //   alignItems: 'center',
-  //   width: '100%',
-  //   height: '45vh',
-  //   padding: '10%',
-  //   borderRadius: 10,
-  //   color: 'white',
-
-  // },
 
   card: {
     display: 'flex',
@@ -26,14 +14,7 @@ export default makeStyles({
    
     color: 'white'
   },
-  // infoCard: {
-  //   display: 'flex', flexDirection: 'column', textAlign: 'center', marginLeft: '20px',
-  // },
-
-  // infoCard: {
-  //   alignItems: 'center'
-  // },
-
+  
   container: {
     padding: '0 5%', width: '100%', margin: 0,
   },
