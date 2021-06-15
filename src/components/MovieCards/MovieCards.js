6f2192f9f222dd9a3da5ccf9e4f6f682b2be5dc7
Aviral Tandon <201951036@iiitvadodara.ac.in>
2021-06-15 20:44:54 +0530

    Update MovieCards.js

--- src/components/MovieCards/MovieCards.js
+++ src/components/MovieCards/MovieCards.js
@@ -3,11 +3,6 @@ import MovieCard from '../MovieCard/MovieCard';
 import { Grid,Grow,Typography } from '@material-ui/core';
 import useStyles from './styles';
 
-// const infoCards = [
-//     { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
-//     { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
-//     { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
-//   ];
 
 const MovieCards = ({movies,activeMovie}) => {
     const classes = useStyles();
